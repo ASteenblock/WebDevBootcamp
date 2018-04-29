c886319fef0f12836e30ab1658e80844202e889e
Alan Steenblock <s00do@Alans-MacBook-Pro.local>
2018-04-29 17:36:51 -0600

    added Script Tag Project folder

---
+++ Script Tag Project/script.js
@@ -0,0 +1,11 @@
+var firstName = prompt("What is your first name");
+var lastName = prompt("What is your last name?"); 
+var age = prompt("How old are you?")
+alert("Nice to meet you, " + firstName + " " + lastName + "." + " " + "Your age is " + age + ".");
+
+
+//ask for the user's first name
+//ask for the user's last name
+//ask for the user's age
+//print out the user's full name in a sentence
+//print out the user's age in a sentence
